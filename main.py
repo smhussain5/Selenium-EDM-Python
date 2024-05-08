b02a2eca9471a119150f307c7150d48f798e9591
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2024-05-08 14:17:43 -0500

    Implement sleep to allow page to load before scraping

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
@@ -13,15 +15,18 @@ driver.get(URL)
 # TODO: FIND 4 LATEST
 latest_button = driver.find_element(By.XPATH, "//yt-formatted-string[@title='Latest']")
 latest_button.click()
+time.sleep(1)
 latest = driver.find_elements(
     By.XPATH, "//ytd-rich-grid-row/div[@id='contents']//a[@id='video-title-link']"
 )
 
 latest_tracks = list()
 latest_tracks_links = list()
+
 for track in latest[:5]:
     latest_tracks.append(track.text)
     latest_tracks_links.append(track.get_attribute("href"))
+
 print(latest_tracks)
 print(latest_tracks_links)
 
@@ -30,15 +35,18 @@ popular_button = driver.find_element(
     By.XPATH, "//yt-formatted-string[@title='Popular']"
 )
 popular_button.click()
+time.sleep(1)
 popular = driver.find_elements(
     By.XPATH, "//ytd-rich-grid-row/div[@id='contents']//a[@id='video-title-link']"
 )
 
 popular_tracks = list()
 popular_tracks_links = list()
+
 for track in popular[:5]:
     popular_tracks.append(track.text)
     popular_tracks_links.append(track.get_attribute("href"))
+
 print(popular_tracks)
 print(popular_tracks_links)
 driver.quit()
